74ff748f5034ba0bdacf12389161ded20751b13f
cameron-mccarthy <cameronsm21@gmail.com>
2025-01-22 19:58:20 -0600

    buttons

--- capstone/src/app/main/main.component.ts
+++ capstone/src/app/main/main.component.ts
@@ -5,12 +5,31 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-main',
-  imports: [RouterModule, MatButtonModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  imports: [RouterModule, MatButtonModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  searchText!: string;
+
+  scan(){
+    console.log("scan")
+  }
+
+  add(){
+    console.log("add")
+  }
+
+  refresh(){
+    console.log("refresh")
+  }
+
+  search(){
+    console.log(this.searchText)
+  }
 
 }
