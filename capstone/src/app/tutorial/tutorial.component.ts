de228095d7353d97671956a1cc02c967f7baf7cb
Carson-36 <94403516+Carson-36@users.noreply.github.com>
2025-02-11 09:23:20 -0600

    Tutotial Page

--- capstone/src/app/tutorial/tutorial.component.ts
+++ capstone/src/app/tutorial/tutorial.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-tutorial',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './tutorial.component.html',
   styleUrl: './tutorial.component.css'
 })
 export class TutorialComponent {
-
+  
 }
