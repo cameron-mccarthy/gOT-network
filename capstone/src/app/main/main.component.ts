1681437b890f70ae3133bcb27561affef948b091
cameron-mccarthy <cameronsm21@gmail.com>
2025-03-18 13:59:17 -0500

    dyanmic header

--- capstone/src/app/main/main.component.ts
+++ capstone/src/app/main/main.component.ts
@@ -4,42 +4,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { DevicesService } from '../devices.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddDeviceDialogComponent } from '../add-device-dialog/add-device-dialog.component';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-main',
-  imports: [RouterModule, MatButtonModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [RouterModule, MatButtonModule, MatSidenavModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
   readonly dialog = inject(MatDialog);
   searchText!: string;
-  constructor(public DevicesService: DevicesService) {
+  constructor(public DevicesService: DevicesService, private router: Router) {
   }
-  scan() {
-    console.log("scan");
-  }
-
-  add() {
-    const dialogRef = this.dialog.open(AddDeviceDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-      if (result){
-        this.DevicesService.addDevice(result);
-      }
-    })
-  }
-
-  refresh() {
-    console.log("refresh")
-  }
-
-  search() {
-    console.log(this.searchText)
-  }
+  
 
 }
