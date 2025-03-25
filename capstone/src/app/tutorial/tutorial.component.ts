719142052dd5c42e6debce74947169dac5f6791a
Carson-36 <94403516+Carson-36@users.noreply.github.com>
2025-03-25 14:07:58 -0500

    tutorial scrolling

--- capstone/src/app/tutorial/tutorial.component.ts
+++ capstone/src/app/tutorial/tutorial.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { ElementRef } from '@angular/core';
+import { ViewChildren } from '@angular/core';
+import { QueryList } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 
 
 @Component({
@@ -9,6 +12,23 @@ import { RouterModule } from '@angular/router';
   templateUrl: './tutorial.component.html',
   styleUrl: './tutorial.component.css'
 })
-export class TutorialComponent {
-  
+export class TutorialComponent implements AfterViewInit{
+  @ViewChildren('sections') sections!: QueryList<ElementRef>;
+
+  private sectionMap = new Map<string, ElementRef>();
+
+  ngAfterViewInit() {
+    // Store each section reference in a map using its ID
+    this.sections.forEach(section => {
+      const id = section.nativeElement.id;
+      this.sectionMap.set(id, section);
+    });
+  }
+
+  scrollToSection(sectionId: string) {
+    const section = this.sectionMap.get(sectionId);
+    if (section) {
+      section.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
