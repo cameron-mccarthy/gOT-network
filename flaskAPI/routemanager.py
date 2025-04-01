0a957b0c3505e7ce6ba64a5cf20d4384013a9c03
Kynlenara <christinasimino@gmail.com>
2025-04-01 14:44:08 -0500

    Update routemanager.py

--- flaskAPI/routemanager.py
+++ flaskAPI/routemanager.py
@@ -20,10 +20,10 @@ def addDev():
         return "This is a GET request!"
     
     if request.method == 'POST':
-        data = request.form
-        print(data)
-    #db.addDevice()
-    return "Figure out post requests"
+        data = request.json
+        params = [data.get('MAC'), data.get('IP'), data.get('Product')]
+        db.addDevice(params[0],params[1],params[2])
+    #return "Figure out post requests"
 
 @app.route('/editDev', methods=['GET', 'POST'])
 def editDev():
