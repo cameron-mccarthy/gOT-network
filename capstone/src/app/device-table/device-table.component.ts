823e6f871d26f8f422779c77274b9910b977a1ff
Carson-36 <94403516+Carson-36@users.noreply.github.com>
2025-04-22 18:34:10 -0500

    table sorting

--- capstone/src/app/device-table/device-table.component.ts
+++ capstone/src/app/device-table/device-table.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { DevicesService } from '../services/devices.service';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Device } from '../interfaces/device';
 import { ActivatedRoute } from '@angular/router';
+import { ViewChild, AfterViewInit } from '@angular/core';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-device-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatSortModule],
   templateUrl: './device-table.component.html',
   styleUrl: './device-table.component.css'
 })
@@ -14,10 +16,19 @@ import { ActivatedRoute } from '@angular/router';
 
 export class DeviceTableComponent {
   ngOnInit(){
-    this.DeviceService.getAllDevices().subscribe(data => this.data = data)
+    this.DeviceService.getAllDevices().subscribe(data => this.dataSource.data = data)
   }
+
+  @ViewChild(MatSort) sort!: MatSort;
+
   displayColumns: string[] = ["IP", "MAC", "Vendor", "Product", "Type", "Status"];
   data!: Device[];
   constructor(public DeviceService: DevicesService){
   }
+  dataSource = new MatTableDataSource<Device>();
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
 }
